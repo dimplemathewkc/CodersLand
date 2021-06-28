b1611f6c1e23018f7698bc4da6898022749fdefd
dimple5 <dimple@vernacular.ai>
2021-06-28 10:55:35 +0530

    removed app.css

--- src/components/editor.jsx
+++ src/components/editor.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import '../App.css';
+// import '../App.css';
 import LangSelection from './languageSelection';
 import Theme from './theme';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
